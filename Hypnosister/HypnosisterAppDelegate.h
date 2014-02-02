bf988a4710a881c55921684183012897e2c8b78e
K-RKidd <kr248196@muhlenberg.edu>
2014-02-01 19:12:19 -0500

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Krystle on 2/1/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
